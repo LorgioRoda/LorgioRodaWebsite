bc10c841955c5c4427a497decb505da43ab549f7
Lorgio Roda <lorgiorodaroca@gmail.com>
2022-06-24 20:35:16 +0200

    update styles

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -13,11 +13,12 @@
             <p class="about_me--text">
                 Then I decided to leave my comfort zone and move to Spain. I was lucky to collaborate and work with very good teams, where I have developed and improved my technical and soft skills, now I want to face new challenges and never stop learning.
             </p>
-            <div class="about_me--social">
-                <a href="https://twitter.com/roda_lorgio" target="_blank">Twitter</a>
-                <a href="https://www.linkedin.com/in/lorgio-roda-roca/" target="_blank">LinkedIn</a>
-                <a href="mailto:lorgiorodaroca@gmail.com">Email</a>
-            </div>
+        </div>
+        <div></div>
+        <div class="about_me--social">
+            <a href="https://twitter.com/roda_lorgio" target="_blank">Twitter</a>
+            <a href="https://www.linkedin.com/in/lorgio-roda-roca/" target="_blank">LinkedIn</a>
+            <a href="mailto:lorgiorodaroca@gmail.com">Email</a>
         </div>
     </div>
 </template>
@@ -26,6 +27,7 @@ export default {};
 </script>
 <style>
 .about_me {
+  height: 100vh;
   font-size: 3vh;
 }
 .about_me h2 {
@@ -51,13 +53,16 @@ export default {};
   margin-top: 40px;
   font-size: 1.7rem;
   letter-spacing: 1.5px;
+  text-align-last: auto;
   line-height: 28px;
 }
 .about_me--social {
   display: flex;
-  justify-content: space-around;
   max-width: 400px;
+  align-items: center;
+  justify-content: space-around;
   margin-top: 60px;
+  margin: 0px 20px;
 }
 .about_me--social a {
   font-size: 1.7rem;
@@ -68,14 +73,15 @@ export default {};
   .about_me {
     display: grid;
     grid-template-columns: 1fr max-content;
+    grid-template-rows: [row1-start] 1fr [row1-end] 1fr;
   }
   .about_me--info {
-      margin: 30px 20px;
-      max-width: 600px;
+    margin: 30px 20px;
+    max-width: 600px;
   }
   .about_me--text {
-      margin: 20px 20px;
-      margin-bottom: 20px;
+    margin: 30px 20px;
+    margin-bottom: 20px;
   }
   .about_me--social {
     max-width: 600px;
@@ -83,6 +89,14 @@ export default {};
   }
   .about_me--social a {
     margin: 0px 20px;
+  }
+  @media (min-width: 2120px) {
+    .about_me--text {
+      max-width: 900px;
+    }
+    .about_me--social {
+      max-width: 900px;
+    }
   }
 }
 </style>
